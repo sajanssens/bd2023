9629cb170e62047155edb6bfeee27ff1480b6613
bramjanssens <bram.janssens@infosupport.com>
2020-10-27 10:24:47 +0100

    JDBC comment added.

--- blok2/jdbcdemo/src/main/java/com/infosupport/JdbcExample.java
+++ blok2/jdbcdemo/src/main/java/com/infosupport/JdbcExample.java
@@ -6,7 +6,11 @@ public class JdbcExample {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
         // 1
-        // Class.forName("com.mysql.cj.jdbc.Driver"); // not necessary anymore since Java.x
+        // Class.forName("com.mysql.cj.jdbc.Driver");
+
+        // Not needed anymore since Java 6 (JDBC4)
+        // "Java6 (and JDBC4) introduced a concept known as "service provider" where implementations of known interface can be detected by the JVM during startup."
+        // https://stackoverflow.com/questions/28220724/class-fornamejdbc-driver-no-longer-needed
 
         // 2
         Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbcdemo", "root", "root");
