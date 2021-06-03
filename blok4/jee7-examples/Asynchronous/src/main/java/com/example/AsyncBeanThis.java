5dfd2514fbedd160a4d9a4df5d38d0260a2fea81
bramjanssens <bram.janssens@infosupport.com>
2021-06-03 14:30:52 +0200

    Misc.

---
+++ blok4/jee7-examples/Asynchronous/src/main/java/com/example/AsyncBeanThis.java
@@ -0,0 +1,43 @@
+package com.example;
+
+import javax.ejb.Asynchronous;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+@Stateless
+public class AsyncBeanThis {
+
+    // @Inject AsyncBeanThis this; // NOT the case!
+
+    @Inject AsyncBeanThis myself;
+
+    public long processWork(String data) throws InterruptedException {
+        System.out.println("[1]");
+        long currentTimeMillisBefore = System.currentTimeMillis();
+        this.doSomeAsyncThing(data);
+        this.doAnotherAsyncThing(data);
+        long currentTimeMillisAfter = System.currentTimeMillis();
+        long elapsedMilliSeconds = currentTimeMillisAfter - currentTimeMillisBefore;
+        System.out.println("[4] elapsed time in milliseconds: " + elapsedMilliSeconds);
+        return elapsedMilliSeconds;
+    }
+
+    @Asynchronous
+    public void doSomeAsyncThing(String data) {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+        }
+        System.out.println("[3] async " + data);
+    }
+
+    @Asynchronous
+    public void doAnotherAsyncThing(String data) {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+
+        }
+        System.out.println("[2] another async " + data);
+    }
+}
